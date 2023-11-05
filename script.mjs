7307ea96bda12b14ce5fcf14e60284967f59e38e
Kaleem Ullah <malikkaleemullah2k@gmail.com>
2023-11-05 12:42:57 +0500

    completed using ternary operator

--- script.mjs
+++ script.mjs
@@ -1,18 +1,23 @@
 function ageChecking(event) {
     event.preventDefault()
     let ageOfPerson = document.querySelector("#age").value 
-    let condition = '';
+    // let condition = '';
+    // (ageOfPerson < 15)? "Child":
+    //     (ageOfPerson < 45)? "young":
+    //         (ageOfPerson < 60)? "Old": "retired"
 
-    if(ageOfPerson <= 15){
-        condition= "Child"
-    }else if(ageOfPerson <= 45){
-        condition= "Young"
-    }else if(ageOfPerson >= 45){
-        condition ="Old"
-    }else {
-        condition= "retired"
-    }
-    console.log(condition)
-    message = `your condition is ${condition}`
+    // if(ageOfPerson <= 15){
+    //     condition= "Child"
+    // }else if(ageOfPerson <= 45){
+    //     condition= "Young"
+    // }else if(ageOfPerson >= 45){
+    //     condition ="Old"
+    // }else {
+    //     condition= "retired"
+    // }
+
+
+    message = `your condition is ${(ageOfPerson < 15)? "Child":(ageOfPerson < 45)? "young":(ageOfPerson < 60)? "Old": "retired"}`
     document.querySelector("#result").innerHTML = message
-}
+    console.log(message)
+}       
